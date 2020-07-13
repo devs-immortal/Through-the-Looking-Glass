f8134b30a7ed50c6f50a278a62dbe9a4224add80
Azazelthedemonlord <spectreneo@gmail.com>
2020-07-12 21:08:32 -0500

    a

---
+++ src/main/java/azzy/fabric/lookingglass/GlassAtlas.java
@@ -0,0 +1,9 @@
+package azzy.fabric.lookingglass;
+
+import net.minecraft.util.Identifier;
+
+import static azzy.fabric.lookingglass.LookingGlass.MODID;
+
+public class GlassAtlas {
+    public static Identifier DYNAMIC_ATLAS = new Identifier(MODID, "textures/atlas/dynamic.png");
+}
--- src/main/java/azzy/fabric/lookingglass/ProjectorRenderer.java
+++ src/main/java/azzy/fabric/lookingglass/ProjectorRenderer.java
@@ -1,6 +1,8 @@
 package azzy.fabric.lookingglass;
 
 import com.mojang.blaze3d.platform.GlStateManager;
+import com.sun.jna.Function;
+import net.fabricmc.fabric.impl.client.indigo.renderer.IndigoRenderer;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumer;
@@ -10,9 +12,17 @@ import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.texture.Sprite;
 import net.minecraft.client.texture.SpriteAtlasTexture;
+import net.minecraft.client.util.SpriteIdentifier;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.client.util.math.Vector3f;
+import net.minecraft.resource.Resource;
 import net.minecraft.util.Identifier;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL13;
+
+import java.io.IOException;
+
+import static azzy.fabric.lookingglass.LookingGlass.FFLog;
 
 public class ProjectorRenderer extends BlockEntityRenderer<ProjectorEntity> {
 
@@ -37,13 +47,23 @@ public class ProjectorRenderer extends BlockEntityRenderer<ProjectorEntity> {
         matrices.multiply(Vector3f.POSITIVE_Z.getDegreesQuaternion(blockEntity.rotZ));
         matrices.scale((float) blockEntity.scale, (float) blockEntity.scale, (float) blockEntity.scale);
 
+
         VertexConsumer consumer = vertexConsumers.getBuffer(RenderLayer.getSolid());
         MatrixStack.Entry matrix = matrices.peek();
 
+        Resource resource = null;
 
-        MinecraftClient.getInstance().getTextureManager().bindTexture(SpriteAtlasTexture.BLOCK_ATLAS_TEX);
         Sprite sprite = MinecraftClient.getInstance().getSpriteAtlas(SpriteAtlasTexture.BLOCK_ATLAS_TEX).apply(new Identifier("minecraft:block/obsidian"));
+        try {
+            resource = MinecraftClient.getInstance().getResourceManager().getResource(new Identifier("minecraft:textures/block/allium.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        java.util.function.Function<Identifier, RenderLayer> layerFactory = a -> RenderLayer.getCutoutMipped();
+
+        SpriteIdentifier spriteIdentifier = new SpriteIdentifier(SpriteAtlasTexture.BLOCK_ATLAS_TEX, new Identifier("minecraft:textures/block/allium.png"));
+        consumer = spriteIdentifier.getVertexConsumer(vertexConsumers, layerFactory);
 
         if (blockEntity.displayState == 0) {
             consumer.vertex(matrix.getModel(), 0, 0, 0).color(255, 255, 255, 255).texture(0, 1).light(14680160).normal(matrix.getNormal(), 1, 1, 1).next();
