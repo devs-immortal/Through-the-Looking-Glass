15bb984d1b8dd77e63d266bde23e7722a0b6ab69
smilodon79 <jayaramv2@hexaware.com>
2021-03-29 10:58:06 -0400

    Interim commit.
    The multiblock now requires 8 display pedestals.
    The multiblock persists its status when the server is restarted.
    The particle effects are working fine, but they keep working for now.
    I need to modify the particle effects to only show during the crafting process.
    
    TODO:
    1.  Implement a solar system like rotating render for the unstable items if possible.
    2.  Implement REI support and an API for unstable crafting.
    3.  Implement the crafting process
    
    Signed-off-by: smilodon79 <jayaramv2@hexaware.com>

--- src/main/java/azzy/fabric/lookingglass/block/DisplayPedestalBlock.java
+++ src/main/java/azzy/fabric/lookingglass/block/DisplayPedestalBlock.java
@@ -5,7 +5,6 @@ import azzy.fabric.lookingglass.LookingGlassConstants;
 import azzy.fabric.lookingglass.blockentity.DisplayPedestalEntity;
 import azzy.fabric.lookingglass.blockentity.LookingGlassBE;
 import azzy.fabric.lookingglass.blockentity.UnstableAltarEntity;
-import azzy.fabric.lookingglass.util.ParticleUtils;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.BlockState;
@@ -14,7 +13,6 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
@@ -24,7 +22,6 @@ import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @SuppressWarnings("deprecated")
@@ -53,7 +50,6 @@ public class DisplayPedestalBlock extends LookingGlassBlock implements BlockEnti
     public void onPlaced(World world, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack itemStack) {
         boolean unstableAltarFound = false;
         BlockPos unstableAltarPos = null;
-        List<BlockPos> pedestalPositionList = new ArrayList<>();
 
         for (int x = -3; x < 4; x += 3) {
             if (unstableAltarFound)
@@ -80,77 +76,14 @@ public class DisplayPedestalBlock extends LookingGlassBlock implements BlockEnti
             return;
         }
 
-        // Found the unstable altar.  Now check in all directions to see if we can find display pedestals.
-        for (int x = -3; x < 4; x += 3) {
-            for (int z = -3; z < 4; z += 3) {
-                // Central spot.  Since we already found the unstable altar, we don't need to check it again.
-                if ((x == 0) && (z == 0))
-                    continue;
-
-                int posX = unstableAltarPos.getX() + x;
-                int posY = unstableAltarPos.getY();
-                int posZ = unstableAltarPos.getZ() + z;
-
-                BlockPos pedestalPos = new BlockPos(posX, posY, posZ);
-                BlockState pedestalBlockState = world.getBlockState(pedestalPos);
-                Block pedestalBlock = pedestalBlockState.getBlock();
-
-                // This block isn't a display pedestal.  Keep moving.
-                if (!LookingGlassBlocks.DISPLAY_PEDESTAL_BLOCK.equals(pedestalBlock))
-                    continue;
-
-                pedestalPositionList.add(pedestalPos);
-            }
-        }
-
-        if (pedestalPositionList.size() < 8) {
-            // We didn't find all 8 pedestals.  Return silently doing nothing.
-            return;
-        }
-
-        List<BlockPos> multiBlockPositionList = new ArrayList<>();
-        multiBlockPositionList.add(unstableAltarPos);
-        multiBlockPositionList.addAll(pedestalPositionList);
-        List<LookingGlassBE> multiBlockTileEntityList = new ArrayList<>();
-        multiBlockTileEntityList.add((LookingGlassBE) world.getBlockEntity(unstableAltarPos));
-
-        for (BlockPos displayPedestalBlockPos : pedestalPositionList) {
-            DisplayPedestalEntity displayPedestalEntity = (DisplayPedestalEntity) world.getBlockEntity(displayPedestalBlockPos);
-            // This shouldn't be occurring either.  Return silently doing nothing.
-            if (displayPedestalEntity == null) {
-                LookingGlassCommon.FFLog.warn("Not able to get display pedestal's tile entity'.  Kindly contact the mod authors at '" + LookingGlassConstants.GITHUB_ISSUE_URL + "' to log an issue.");
-                return;
-            }
-            multiBlockTileEntityList.add(displayPedestalEntity);
-        }
         UnstableAltarEntity unstableAltarEntity = (UnstableAltarEntity) world.getBlockEntity(unstableAltarPos);
-        // This shouldn't be occurring either.  Return silently doing nothing.
+        // Shouldn't occur in normal course.  Return quietly doing nothing.
         if (unstableAltarEntity == null) {
             LookingGlassCommon.FFLog.warn("Not able to get unstable altar's tile entity.  Kindly contact the mod authors at '" + LookingGlassConstants.GITHUB_ISSUE_URL + "' to log an issue.");
             return;
         }
-        // Set the flag that the multiblock has been formed! - STARTS
-        unstableAltarEntity.isMultiBlockFormed = true;
-        unstableAltarEntity.multiBlockBlockEntitiesList = multiBlockTileEntityList;
-        unstableAltarEntity.multiBlockPositionsList = multiBlockPositionList;
 
-        for (LookingGlassBE lookingGlassBE : multiBlockTileEntityList) {
-            if (lookingGlassBE instanceof DisplayPedestalEntity) {
-                DisplayPedestalEntity displayPedestalEntity = (DisplayPedestalEntity) lookingGlassBE;
-                displayPedestalEntity.isMultiBlockFormed = true;
-                displayPedestalEntity.multiBlockBlockEntitiesList = multiBlockTileEntityList;
-                displayPedestalEntity.multiBlockPositionsList = multiBlockPositionList;
-            }
-        }
-        // Set the flag that the multiblock has been formed! - ENDS
-
-        // Celebratory particle effects.
-        if (!world.isClient) {
-            ParticleUtils.spawnParticles(ParticleUtils.GREEN, ((ServerWorld) world), unstableAltarPos, 1.2, 30);
-            for (BlockPos pedestalPos : pedestalPositionList) {
-                ParticleUtils.spawnParticles(ParticleUtils.GREEN, ((ServerWorld) world), pedestalPos, 1.2, 30);
-            }
-        }
+        unstableAltarEntity.populateMultiBlockPositions();
     }
 
     @Override
--- src/main/java/azzy/fabric/lookingglass/blockentity/UnstableAltarEntity.java
+++ src/main/java/azzy/fabric/lookingglass/blockentity/UnstableAltarEntity.java
@@ -1,8 +1,13 @@
 package azzy.fabric.lookingglass.blockentity;
 
+import azzy.fabric.lookingglass.LookingGlassCommon;
+import azzy.fabric.lookingglass.LookingGlassConstants;
 import azzy.fabric.lookingglass.block.LookingGlassBlocks;
 import azzy.fabric.lookingglass.util.ParticleUtils;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Tickable;
 import net.minecraft.util.math.BlockPos;
@@ -12,8 +17,99 @@ import java.util.List;
 
 public class UnstableAltarEntity extends LookingGlassBE implements Tickable {
     public boolean isMultiBlockFormed = false;
-    public List<BlockPos> multiBlockPositionsList = new ArrayList<>();
-    public List<LookingGlassBE> multiBlockBlockEntitiesList = new ArrayList<>();
+    public List<BlockPos> multiBlockPositionsList = null;
+    public List<LookingGlassBE> multiBlockBlockEntitiesList = null;
+
+    @Override
+    public CompoundTag toTag(CompoundTag tag) {
+        super.toTag(tag);
+        tag.putBoolean("isMultiBlockFormed", isMultiBlockFormed);
+        return tag;
+    }
+
+    @Override
+    public void fromTag(BlockState state, CompoundTag tag) {
+        super.fromTag(state, tag);
+
+        isMultiBlockFormed = tag.getBoolean("isMultiBlockFormed");
+    }
+
+    public void populateMultiBlockPositions() {
+        // Shouldn't normally occur.
+        if (world == null)
+            return;
+
+        List<BlockPos> pedestalPositionList = new ArrayList<>();
+
+        // Found the unstable altar.  Now check in all directions to see if we can find display pedestals.
+        for (int x = -3; x < 4; x += 3) {
+            for (int z = -3; z < 4; z += 3) {
+                // Central spot.  Since we already found the unstable altar, we don't need to check it again.
+                if ((x == 0) && (z == 0))
+                    continue;
+
+                int posX = pos.getX() + x;
+                int posY = pos.getY();
+                int posZ = pos.getZ() + z;
+
+                BlockPos pedestalPos = new BlockPos(posX, posY, posZ);
+                BlockState pedestalBlockState = world.getBlockState(pedestalPos);
+                Block pedestalBlock = pedestalBlockState.getBlock();
+
+                // This block isn't a display pedestal.  Keep moving.
+                if (!LookingGlassBlocks.DISPLAY_PEDESTAL_BLOCK.equals(pedestalBlock))
+                    continue;
+
+                pedestalPositionList.add(pedestalPos);
+            }
+        }
+
+        // TODO:  Change this to be a configuration driven setting, since this forces us to need 8 pedestals to complete the multiblock.
+        if (pedestalPositionList.size() < 8) {
+            // We didn't find all 8 pedestals.  Return silently doing nothing.
+            isMultiBlockFormed = false;
+            return;
+        }
+
+        List<BlockPos> multiBlockPositionList = new ArrayList<>();
+        multiBlockPositionList.add(pos);
+        multiBlockPositionList.addAll(pedestalPositionList);
+        List<LookingGlassBE> multiBlockTileEntityList = new ArrayList<>();
+        multiBlockTileEntityList.add((LookingGlassBE) world.getBlockEntity(pos));
+
+        for (BlockPos displayPedestalBlockPos : pedestalPositionList) {
+            DisplayPedestalEntity displayPedestalEntity = (DisplayPedestalEntity) world.getBlockEntity(displayPedestalBlockPos);
+            // This shouldn't be occurring either.  Return silently doing nothing.
+            if (displayPedestalEntity == null) {
+                isMultiBlockFormed = false;
+                LookingGlassCommon.FFLog.warn("Not able to get display pedestal's tile entity'.  Kindly contact the mod authors at '" + LookingGlassConstants.GITHUB_ISSUE_URL + "' to log an issue.");
+                return;
+            }
+            multiBlockTileEntityList.add(displayPedestalEntity);
+        }
+        // Set the flag that the multiblock has been formed! - STARTS
+        isMultiBlockFormed = true;
+        multiBlockBlockEntitiesList = multiBlockTileEntityList;
+        multiBlockPositionsList = multiBlockPositionList;
+
+        for (LookingGlassBE lookingGlassBE : multiBlockTileEntityList) {
+            if (lookingGlassBE instanceof DisplayPedestalEntity) {
+                DisplayPedestalEntity displayPedestalEntity = (DisplayPedestalEntity) lookingGlassBE;
+                displayPedestalEntity.isMultiBlockFormed = true;
+                displayPedestalEntity.multiBlockBlockEntitiesList = multiBlockTileEntityList;
+                displayPedestalEntity.multiBlockPositionsList = multiBlockPositionList;
+            }
+        }
+        // Set the flag that the multiblock has been formed! - ENDS
+
+        // Celebratory particle effects.
+        if (!world.isClient) {
+            ParticleUtils.spawnParticles(ParticleUtils.GREEN, ((ServerWorld) world), pos, 1.2, 30);
+            for (BlockPos pedestalPos : pedestalPositionList) {
+                ParticleUtils.spawnParticles(ParticleUtils.GREEN, ((ServerWorld) world), pedestalPos, 1.2, 30);
+            }
+        }
+    }
 
     public UnstableAltarEntity() {
         super(LookingGlassBlocks.UNSTABLE_ALTAR_ENTITY, 1);
@@ -24,6 +120,11 @@ public class UnstableAltarEntity extends LookingGlassBE implements Tickable {
         if (!isMultiBlockFormed || (world == null))
             return;
 
+        // Only occurs on server reload.  I thought about persisting this data, but ultimately decided against it since it's not really necessary to persist.
+        // This method also resets the isMultiBlockFormed to false if there's an issue detecting the multiblock, to avoid log spam.
+        if ((multiBlockBlockEntitiesList == null) || (multiBlockBlockEntitiesList.isEmpty()))
+            populateMultiBlockPositions();
+
         ItemStack inventory = getStack(0);
         if (inventory.isEmpty()) {
             return;
@@ -33,7 +134,8 @@ public class UnstableAltarEntity extends LookingGlassBE implements Tickable {
             // Shouldn't ever happen.  But weirder things have happened.  Here, I'm checking to see if the multiblock only ever needs the unstable altar
             // (I'm thinking about make the # of required display pedestals configurable.
             // This is just a fail-safe check.
-            if (multiBlockBlockEntitiesList.size() < 2)
+            // TODO:  Modify this to be a configuration driven setting to allow for a configurable number of pedestals in the multiblock.  This number will be unstable altar + # of pedestals.
+            if (multiBlockBlockEntitiesList.size() < 9)
                 return;
 
             for (int i = 1; i < multiBlockBlockEntitiesList.size(); i++) {
